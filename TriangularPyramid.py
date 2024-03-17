39c19080d31ad6a9f1c0a5f8e96049a4acd0fda9
Nikita Shestopal <snikita2206@gmail.com>
2024-03-17 13:20:40 +0200

    Fix all isues with TriangularPyramid

--- TriangularPyramid.py
+++ TriangularPyramid.py
@@ -3,10 +3,13 @@ from Triangle import Triangle
 class TriangularPyramid(Triangle):
     def __init__(self, a, h):
         super().__init__(a, a, a)
-        self.height = h
+        self.h = h
 
     def dimension(self):
         return '3D'
 
     def volume(self):
-        return (1 / 3) * super().square() * self.height
+        base_area = super().square()
+        if base_area is None:
+            return None
+        return (1 / 3) * base_area * self.h
